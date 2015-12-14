f4aebfa8cf648864bb74c2e61008bd7416dc4911
Varal7 <varaal@gmail.com>
2015-12-14 15:54:38 +0100

    Intialized Django Project

---
+++ skyprint/deformation/objects/parabole.py
@@ -0,0 +1,11 @@
+from point import *
+
+
+class Parabole():
+    def __init__(self):
+        origin = Point()
+        time_at_explosion = 0
+        phi = 0
+        theta = 0
+        initial_velocity = 0
+        
---
+++ skyprint/deformation/objects/point.py
@@ -0,0 +1,8 @@
+class Point:
+    """ Point class represents and manipulates x,y coords. """
+
+
+    def __init__(self):
+        """ Create a new point at the origin """
+        self.x = 0
+        self.y = 0
