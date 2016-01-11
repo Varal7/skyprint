d060ab54fa8ef0ef65bd0c7fad93c9860e4c811b
Varal7 <varaal@gmail.com>
2016-01-11 18:09:20 +0100

    Added first pages

--- skyprint/deformation/urls.py
+++ skyprint/deformation/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.home, name='home'),
+        url(r'^input', views.input, name='input'),
+        url(r'^all', views.all, name='all'),
+
         ]
--- skyprint/deformation/views.py
+++ skyprint/deformation/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return  render(request, 'deformation/home.html', {})
+
+def input(request):
+    return  render(request, 'deformation/input.html', {})
+
+def all(request):
+    return  render(request, 'deformation/all.html', {})
